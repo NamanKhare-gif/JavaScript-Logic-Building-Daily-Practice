21be74adc6cdf16bd735bb8a6bb01ab9aa265481
Naman Khare <namankhare329@gmail.com>
2026-09-08 16:49:44 +0530

    Add the Checking function which checks the number is palindrome or not

--- P-1/script.js
+++ P-1/script.js
@@ -1,4 +1,6 @@
 const checkBtn1 = document.querySelector(".Check-btn");
+const checkBtn2 = document.querySelector(".check-btn2");
+const SuccessDiv = document.querySelector(".Successfull");
 const numInp = document.querySelector("#number-input");
 
 checkBtn1.addEventListener("click", () => {
@@ -11,7 +13,47 @@ checkBtn1.addEventListener("click", () => {
     alert("Please enter numbers only.");
     return;
   }
-  checkBtn1.textContent = "Checking . . . .";
-  console.log(value);
-  // checking();
+  else{
+    checkBtn1.textContent = "Checking . . . .";
+    console.log(value);
+    checking(value);
+  }
+  
 });
+
+checkBtn2.addEventListener("click", () => {
+  const value = numInp.value.trim();
+  if (value === "") {
+    alert("Please enter a number.");
+    return;
+  }
+  if (!/^\d+$/.test(value)) {
+    alert("Please enter numbers only.");
+    return;
+  }
+  else{
+    checkBtn1.textContent = "Checking . . . .";
+    console.log(value);
+    checking(value);
+  }
+  
+});
+
+function checking(value) {
+
+    setTimeout(() => {
+
+        const reversed = value.split("").reverse().join("");
+
+        if (value === reversed) {
+            console.log("Yes! It is a palindrome.");
+            checkBtn1.style.display = "none";
+            checkBtn2.style.display = "flex";
+            SuccessDiv.style.display = "inline-block";
+        } else {
+            console.log("No! It is not a palindrome.");
+        }
+
+    }, 1000);
+
+}
