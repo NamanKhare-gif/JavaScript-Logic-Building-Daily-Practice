ec84363a8eb366cc88ecaafe73b445b1af285cb2
Naman Khare <namankhare329@gmail.com>
2026-09-08 17:48:51 +0530

    Add the Error showing design and write the javaScript for the all design

--- P-1/script.js
+++ P-1/script.js
@@ -1,6 +1,10 @@
 const checkBtn1 = document.querySelector(".Check-btn");
 const checkBtn2 = document.querySelector(".check-btn2");
+const checkBtn3 = document.querySelector(".check-btn3");
 const SuccessDiv = document.querySelector(".Successfull");
+const UnsuccessDiv = document.querySelector(".Unsuccessfull");
+const number1 = document.querySelector(".number1");
+const number2 = document.querySelector(".number2");
 const numInp = document.querySelector("#number-input");
 
 checkBtn1.addEventListener("click", () => {
@@ -39,8 +43,27 @@ checkBtn2.addEventListener("click", () => {
   
 });
 
-function checking(value) {
+checkBtn3.addEventListener("click", () => {
+  const value = numInp.value.trim();
+  if (value === "") {
+    alert("Please enter a number.");
+    return;
+  }
+  if (!/^\d+$/.test(value)) {
+    alert("Please enter numbers only.");
+    return;
+  }
+  else{
+    checkBtn1.textContent = "Checking . . . .";
+    console.log(value);
+    checking(value);
+  }
+  
+});
 
+function checking(value) {
+    number1.textContent = value;    
+    number2.textContent = value;
     setTimeout(() => {
 
         const reversed = value.split("").reverse().join("");
@@ -50,8 +73,15 @@ function checking(value) {
             checkBtn1.style.display = "none";
             checkBtn2.style.display = "flex";
             SuccessDiv.style.display = "inline-block";
+            checkBtn3.style.display = "none";
+            UnsuccessDiv.style.display = "none"
         } else {
             console.log("No! It is not a palindrome.");
+            SuccessDiv.style.display = "none";
+            checkBtn1.style.display = "none";
+            checkBtn2.style.display = "none";
+            checkBtn3.style.display = "flex";
+            UnsuccessDiv.style.display = "inline-block"
         }
 
     }, 1000);
