c61ce677a9850889c3e0dc39122ac31b01759e9c
Naman Khare <namankhare329@gmail.com>
2026-09-10 18:49:12 +0530

    Start adding the JavaScript

---
+++ P-2/script.js
@@ -0,0 +1,6 @@
+const genrtBtn = document.querySelector("#generateBtn");
+const hexValue = document.querySelector("#hexValue");
+
+genrtBtn.addEventListener("click",()=>{
+    hexValue.textContent = "Click";
+});
