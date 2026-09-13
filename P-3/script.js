37af7b446ea2687069978a6a47b6e30cfff54cb1
Naman Khare <namankhare329@gmail.com>
2026-09-13 11:50:08 +0530

    Add the toggle class list name is-active

--- P-3/script.js
+++ P-3/script.js
@@ -1,3 +1,19 @@
-const firstColorBtn = document.querySelector("#first color");
-const secondColorBtn = document.querySelector("#second color");
-const thirdColorBtn = document.querySelector("#third color");
+const firstColorBtn = document.querySelector("#firstColor");
+const secondColorBtn = document.querySelector("#secondColor");
+const thirdColorBtn = document.querySelector("#thirdColor");
+
+const colorButtons = [
+    firstColorBtn,
+    secondColorBtn,
+    thirdColorBtn
+];
+
+colorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        colorButtons.forEach((btn) => {
+            btn.classList.remove("is-active");
+        });
+        button.classList.add("is-active");
+    });
+
+});
