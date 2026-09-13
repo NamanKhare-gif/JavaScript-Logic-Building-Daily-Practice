e36bd918876d4d1452d7702b428ffb481398d96b
Naman Khare <namankhare329@gmail.com>
2026-09-13 21:51:21 +0530

    add the Gallery selector funtionality

--- P-3/script.js
+++ P-3/script.js
@@ -1,6 +1,8 @@
 const firstColorBtn = document.querySelector("#firstColor");
 const secondColorBtn = document.querySelector("#secondColor");
 const thirdColorBtn = document.querySelector("#thirdColor");
+const mainImg = document.querySelector(".Main-img");
+const galleryThumbs = document.querySelectorAll(".gallery__thumb");
 
 const colorButtons = [
     firstColorBtn,
@@ -14,6 +16,30 @@ colorButtons.forEach((button) => {
             btn.classList.remove("is-active");
         });
         button.classList.add("is-active");
+    });
+
+});
+
+galleryThumbs.forEach((thumb) => {
+
+    thumb.addEventListener("click", () => {
+
+        // Remove is-active from all thumbnail buttons
+        galleryThumbs.forEach((item) => {
+            item.classList.remove("is-active");
+        });
+
+        // Add is-active to the clicked thumbnail
+        thumb.classList.add("is-active");
+
+        // Find the image inside the clicked button
+        const selectedImg = thumb.querySelector("img");
+
+        // Change the main image
+        if (selectedImg) {
+            mainImg.src = selectedImg.src;
+        }
+
     });
 
 });
